ca9a257ea6ce81fe07a6da7eecc99b3a4ef553c0
wisdomabioye <wisdomabioye@gmail.com>
2025-11-22 16:11:14 +0100

    feat: update theme

---
+++ components/showcase/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { ThemeShowcase } from "./theme";
+import { GradientShowcase } from "./gradient"
+
+
+export function ShowcasePage() {
+
+    return (
+        <>
+            <GradientShowcase />
+            <ThemeShowcase />
+        </>
+    )
+}
